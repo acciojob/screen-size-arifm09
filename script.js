730cf800c6a0dcbc496cecf74994f0e2916b8301
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-05 09:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let widthSpan = document.getElementById("width");
+let heightSpan = document.getElementById("height");
+
+var onresize = function() {
+   //your code here
+   //this is just an example
+
+	let width = widthSpan.innerText;
+	let height = heightSpan.innerText;
+   width = document.body.clientWidth;
+   height = document.body.clientHeight;
+
+	widthSpan.innerText = width;
+	heightSpan.innerText = height;
+}
+window.addEventListener("resize", onresize);
